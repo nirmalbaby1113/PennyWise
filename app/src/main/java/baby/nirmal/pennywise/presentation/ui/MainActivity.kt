d697f76b2b659f213371d62ae4ec08e1217b13e2
Nirmal Baby <nirmalbaby@dhcp-9-28-251-234.hal.can.ibm.com>
2025-05-29 10:32:47 -0300

    Created a basic project structure (MVVM)

--- app/src/main/java/baby/nirmal/pennywise/presentation/ui/MainActivity.kt
+++ app/src/main/java/baby/nirmal/pennywise/presentation/ui/MainActivity.kt
@@ -1,10 +1,9 @@
-package baby.nirmal.pennywise
+package baby.nirmal.pennywise.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import baby.nirmal.pennywise.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
